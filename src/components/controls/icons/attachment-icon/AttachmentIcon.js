d7df2304d48c99997ba5794cf926caf34ccb92e1
Nourhan Saieed <nourhansaieed@Nourhans-MacBook-Pro.local>
2020-05-29 12:21:09 +0200

    handle the existing code

---
+++ src/components/controls/icons/attachment-icon/AttachmentIcon.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./AttachmentIcon.scss";
+
+const AttachmentIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="22">
+      <text y="20" fill="#666" font-family="Gotham" font-size="32">
+        +
+      </text>
+    </svg>
+  );
+};
+
+export default AttachmentIcon;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import createSagaMiddleware from "redux-saga";
+
+import "./index.scss";
+import rootSaga from "./store/sagas";
+import rootReducer from "./store/reducers";
+import App from "./App";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(sagaMiddleware));
+
+const store = createStore(rootReducer, enhancer);
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
